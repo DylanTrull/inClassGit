cbd073f4feda1d49de5f8cf9755e70c33ca1f945
DylanTrull <trulld@oregonstate.edu>
2021-04-15 17:08:21 -0700

    Added password generator

--- password.py
+++ password.py
@@ -1,10 +1,12 @@
 import random
 import string
 
+
 def randomPassword(n):
 	randLetter = string.ascii_letters
 
 	password = ''.join(random.choice(randLetter) for i in range(n))
 	print(password)
 
-randomPassword(10)
+num = input("Password Length")
+randomPassword(num)
