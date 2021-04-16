6145e878d37ccd382b5f64f8f42b6bf2ce2d9a38
DylanTrull <trulld@oregonstate.edu>
2021-04-16 11:15:50 -0700

    Updated Password to force input as int and different combinations of passwords, also skeletal of Divisor

--- password.py
+++ password.py
@@ -3,10 +3,11 @@ import string
 
 
 def randomPassword(n):
-	randLetter = string.ascii_letters
+	randLetter = string.ascii_letters + string.digits
 
 	password = ''.join(random.choice(randLetter) for i in range(n))
 	print(password)
 
-num = input("Password Length")
+num = input("Password Length ")
+num = int(num)
 randomPassword(num)
